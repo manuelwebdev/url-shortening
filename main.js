d24cef6c773a88b9177bd850f1eff93ee6f12074
manoy96 <mfewcar@gmail.com>
2022-03-05 15:27:34 -0700

    Working on input fields javascript.

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
+// Mobile menu
 const mobileMenu = document.querySelector(".menuWrap")
 const hamburger = document.querySelector(".icon")
 
 const hamburgerClick = () => {
   mobileMenu.classList.toggle("disabled")
   hamburger.classList.toggle("hamActive")
+}
+
+// TODO fetch api to shorten url
+const apiUrl = `https://api.shrtco.de/v2/`
+const urlInput = document.querySelector(".urlInput")
+const urlSubmit = document.querySelector("urlSubmit")
+
+const handleSubmit = () => {
+  console.log(urlInput.value)
 }
